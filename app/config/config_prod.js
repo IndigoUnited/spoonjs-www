ae74e54c0b5080a1515be22570468c01d3c7d582
André Cruz <andremiguelcruz@msn.com>
2013-10-14 09:17:14 +0100

    Things I forgot to commit at the lxjs conf.

--- app/config/config_prod.js
+++ app/config/config_prod.js
@@ -4,11 +4,12 @@ define(['./config', 'mout/object/merge'], function (config, merge) {
 
     return merge(config, {
         env: 'prod',
-        version: 2,
+        version: 20,
 
         // Address overrides
         address: {
-            html5: true         // Setup prettier URLs by enabling HTML5
+            basePath: '/spoonjs',
+            html5: false        // Setup prettier URLs by enabling HTML5
                                 // If changed to true, the server needs to be able to rewrite URLs to the front controller
         }
     });
